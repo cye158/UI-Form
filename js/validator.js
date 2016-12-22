07c1bf022759774a4f6c9a8c00f33a255e365211
Clarence Enriquez <cye0105@mail.sfsu.edu>
2016-12-22 15:27:08 -0800

    testing img

--- js/validator.js
+++ js/validator.js
@@ -147,18 +147,18 @@ $(document).ready(function() {
 
 
   function readURL(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
+         if (input.files && input.files[0]) {
+             var reader = new FileReader();
 
-      reader.onload = function (e) {
-        $('#blah')
-        .attr('src', e.target.result)
-        .width(150)
-        .height(200);
-      };
+             reader.onload = function (e) {
+                 $('#issue_img')
+                     .attr('src', e.target.result)
+                     .width(150)
+                     .height(200);
+             };
 
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
+             reader.readAsDataURL(input.files[0]);
+         }
+     }
 
 });
