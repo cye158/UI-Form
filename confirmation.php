59393698e0127b1cdbb11dd6534ef39fc64d99ec
Clarence Enriquez <cye0105@mail.sfsu.edu>
2016-12-26 03:03:08 -0800

    testing all output

--- confirmation.php
+++ confirmation.php
@@ -267,7 +267,7 @@ var address     = street + city + state + zip;
                     <label class="col-md-4 control-label">Additional details: </label>
                     <div class="col-md-7 inputGroupContainer">
                       <div class="input-group">
-                        <textarea disabled class="form-control" disabled value="<?php echo $comment; ?>"></textarea>
+                        <input class="form-control" disabled value="<?php echo $comment; ?>">
                       </div>
                     </div>
                   </div>
