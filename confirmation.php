f4848d6fe9b92251e164bce39a23c0dfb1b57fbe
Clarence Enriquez <cye0105@mail.sfsu.edu>
2016-12-26 02:01:42 -0800

    testing if map works

--- confirmation.php
+++ confirmation.php
@@ -191,7 +191,32 @@
                 <div class="form-group">
                   <label class="col-md-4 control-label">Google Maps:</label>
                   <div class="col-md-7 inputGroupContainer">
-                    <div id="map" onload="initLoc("<?php echo $location; ?>")"></div>
+                    <div id="map"></div>
+                    <script>
+                    function initMap() {
+                      var map = new google.maps.Map(document.getElementById('map'), {
+                        zoom: 14,
+                        center: {lat: 37.7219, lng: -122.4782}
+                      });
+                      var location = "<?php echo $location; ?>";
+                      var geocoder = new google.maps.Geocoder();
+                      geocodeAddress(geocoder, map, location)
+                    }
+
+                    function geocodeAddress(geocoder, resultsMap, location) {
+                      geocoder.geocode({'address': address}, function(results, status) {
+                        if (status === 'OK') {
+                          resultsMap.setCenter(results[0].geometry.location);
+                          var marker = new google.maps.Marker({
+                            map: resultsMap,
+                            position: results[0].geometry.location
+                          });
+                        } else {
+                          alert('Geocode was not successful for the following reason: ' + status);
+                        }
+                      });
+                    }
+                    </script>
                   </div>
                 </div>
               </div>
