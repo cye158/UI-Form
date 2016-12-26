ac5253a63c311036de0981c43f98a7c59ce179d2
Clarence Enriquez <cye0105@mail.sfsu.edu>
2016-12-26 00:11:44 -0800

    testing if works

--- index.php
+++ index.php
@@ -338,7 +338,7 @@
 	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 	<script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'></script>
 	<script src='http://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.4.5/js/bootstrapvalidator.min.js'></script>
-	<script type="text/javascript" src="../js/form.js"></script>
+	<script type="text/javascript" src="./js/form.js"></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.0/jquery-ui.min.js"></script>
 
 </body>
